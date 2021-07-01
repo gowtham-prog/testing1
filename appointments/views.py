fc0630199fadafbc2839fea8d716ab0a1f97d430
Gowtham vulluri <gowthamkumarvulluri@gmail.com>
2021-07-01 12:15:35 +0530

    djaango file

---
+++ appointments/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render,HttpResponse,HttpResponseRedirect
+from django.urls import reverse
+from .models import *
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+
+# Create your views here.
+def index(request):
+    return render(request,"appointments/layout.html")
+def book(request):
+    if request.method =="POST":
+        return HttpResponse("its working")
+    else:
+        return render(request,"appointments/appointment.html")
+def login(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(request, username=email, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "mail/login.html", {
+                "message": "Invalid email and/or password."
+            })
+    else:
+        return render(request, "appointments/login.html")
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+def register(request):
+   if request.method == "POST":
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "mail/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(email, email, password)
+            user.save()
+        except IntegrityError as e:
+            print(e)
+            return render(request, "appointments/register.html", {
+                "message": "Email address already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+   else:
+        return render(request, "appointments/register.html")
